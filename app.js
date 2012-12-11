28d0480057270111dfc223febea4512f51719713
Christian Herlambang <christian.herlambang@fiz-karlsruhe.de>
2012-12-11 14:25:03 +0100

    foo

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var foo = function foo() {
   console.log('hey');
 };
 
+/***/
 window.onload = foo;
 
 assert(typeof window.foo === 'function', 'foo is a function');
@@ -17,8 +18,8 @@ console.log('values: ' + values);
 values.sort();
 console.log('sorted values Ascending: ' + values);
 
-values.sort(function(val1, val2){
-  return val2-val1;
+values.sort(function(val1, val2) {
+  return val2 - val1;
 });
 
 console.log('sorted values Descending: ' + values);
@@ -32,9 +33,11 @@ assert(typeof bar === 'function', 'bar is a function');
 assert(typeof window.bar === 'function', 'window.bar is defined.');
 
 function outer() {
-  assert(typeof inner === 'function', 'inner() is in scope, also before declaration.');
-  function inner(){}
-  assert(typeof inner === 'function', 'inner() is in scope, after declaration.');
+  assert(typeof inner === 'function', 'inner() is in scope, also before' +
+         'declaration.');
+  function inner() {}
+  assert(typeof inner === 'function',
+         'inner() is in scope, after declaration.');
 }
 
 outer();
@@ -42,3 +45,13 @@ assert(typeof inner === undefined, 'inner is not in global scope.');
 
 // scoping and function
 assert(true, 'true');
+
+var local = {};
+
+local2 = {};
+
+assert(window.local, 'local is global');
+
+assert(window.local2, 'local2 is global');
+
+console.log('what is this?', this);
