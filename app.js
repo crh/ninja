10fa054e0d42e353ccdfa30ccd5ff7b9d03ab677
Christian Herlambang <christian.herlambang@fiz-karlsruhe.de>
2013-01-08 18:03:18 +0700

    add updating snoopy

--- app.js
+++ app.js
@@ -1,21 +1,69 @@
-$(document).ready(function(){
-    var REST_URI = 'http://141.66.8.240:8081/aas/organizations';
+$(document).ready(function() {
+    var AAS_URI = 'http://141.66.8.240:8081/aas/persons/';
+    var snoopy;
 
     $.ajax({
       type: 'GET',
-      url: REST_URI,
+      url: AAS_URI + 'snoopy',
       dataType: 'json',
       xhrFields: {
         withCredentials: true
+      },
+      statusCode: {
+        404: function() {
+          console.log('page not found.');
+        },
+        401: function() {
+          console.log('unauthorized.');
+        }
       }
     })
     .done(function(data) {
       console.log('data: ', data);
-      $('#aas').append('success');
+      snoopy = data;
+      $('#aas').append('GET snoopy is successful');
     })
-    .error(function(data) {
-      console.log('error: ', data);
-      $('#aas').append(data.statusText + '\n');
+    .error(function(jqXHR, textStatus, errorThrown) {
+      console.log('error: ', errorThrown);
+      console.log(jqXHR);
+      $('#aas').append(jqXHR.statusText + '\n');
+      if (jqXHR.readyState === 0 && jqXHR.status === 0) {
+        window.open(AAS_URI);
+      }
+    })
+    .always(function(data) {
+      console.log('finsihed: ', data);
+    });
+
+    // Update snoopy
+    $.ajax({
+      type: 'PUT',
+      url: AAS_URI + 'snoopy',
+      async: false,
+      data: snoopy,
+      contentType: 'application/json',
+      dataType: 'json',
+      xhrFields: {
+        withCredentials: true
+      },
+      statusCode: {
+        404: function() {
+          console.log('page not found.');
+        },
+        401: function() {
+          console.log('unauthorized.');
+        }
+      }
+    })
+    .done(function(data) {
+      console.log('data: ', data);
+      $('#aas').append('UPDATE snoopy is successful');
+    })
+    .error(function(jqXHR, textStatus, errorThrown) {
+      console.log('error: ', errorThrown);
+      console.log(jqXHR);
+
+      $('#aas').append(', but UPDATE snoopy failed. ');
     })
     .always(function(data) {
       console.log('finsihed: ', data);
