807da09738cee2e37198e25f3dc1c4c40d241344
Christian Herlambang <christian.herlambang@fiz-karlsruhe.de>
2013-01-08 22:50:35 +0700

    refactor code

--- app.js
+++ app.js
@@ -1,88 +1,120 @@
+var AAS_URI = 'http://141.66.8.240:8081/aas/persons/';
+var snoopy;
+
 $(document).ready(function() {
-    var AAS_URI = 'http://141.66.8.240:8081/aas/persons/';
-    var snoopy;
-
-    $.ajax({
-      type: 'GET',
-      url: AAS_URI + 'snoopy',
-      dataType: 'json',
-      xhrFields: {
-        withCredentials: true
-      },
-      statusCode: {
-        404: function() {
-          console.log('page not found.');
-        },
-        401: function() {
-          console.log('unauthorized.');
-        }
-      }
-    })
-    .done(function(data) {
-      console.log('data: ', data);
-      snoopy = data;
-      $('#aas').append('GET snoopy is successful');
-    })
-    .error(function(jqXHR, textStatus, errorThrown) {
-      console.log('error: ', errorThrown);
-      console.log(jqXHR);
-      $('#aas').append(jqXHR.statusText + '\n');
-      if (jqXHR.readyState === 0 && jqXHR.status === 0) {
-        window.open(AAS_URI);
-      }
-    })
-    .always(function(data) {
-      console.log('finsihed: ', data);
-    });
-
-    // Update snoopy
-    $.ajax({
-      type: 'PUT',
-      url: AAS_URI + 'snoopy',
-      async: false,
-      data: snoopy,
-      contentType: 'application/json',
-      dataType: 'json',
-      xhrFields: {
-        withCredentials: true
-      },
-      statusCode: {
-        404: function() {
-          console.log('page not found.');
-        },
-        401: function() {
-          console.log('unauthorized.');
-        }
-      }
-    })
-    .done(function(data) {
-      console.log('data: ', data);
-      $('#aas').append('UPDATE snoopy is successful');
-    })
-    .error(function(jqXHR, textStatus, errorThrown) {
-      console.log('error: ', errorThrown);
-      console.log(jqXHR);
-
-      $('#aas').append(', but UPDATE snoopy failed. ');
-    })
-    .always(function(data) {
-      console.log('finsihed: ', data);
-    });
-
-    var REST_URI = 'https://api.github.com';
-    var xhr = $.ajax({
-      url: REST_URI,
-      dataType: 'json'
-    })
-    .done(function(data) {
-      console.log('data: ', data);
-      $('#github').append('success');
-    })
-    .error(function(data) {
-      console.log('error: ', data);
-      $('#github').append(data.statusText + '\n');
-    })
-    .always(function(data) {
-      console.log('finsihed: ', data);
-    });
+  getCurrentUser();
 });
+
+var getCurrentUser = function() {
+  $.ajax({
+    type: 'GET',
+    url: AAS_URI + 'current',
+    dataType: 'json',
+    xhrFields: {
+      withCredentials: true
+    },
+    username: 'admin',
+    password: 'admin',
+    statusCode: {
+      404: function() {
+        console.log('page not found.');
+      },
+      401: function() {
+        console.log('unauthorized.');
+      }
+    }
+  })
+  .done(function(data) {
+    console.log('data: ', data);
+    $('.message').text('You are logged in as: ' + data.id);
+    $('.message').append('<div>Getting person with the username snoopy...</div>');
+    findPersonById('snoopy');
+  })
+  .always(function(jqXHR) {
+    console.log('get current user is finished', jqXHR);
+
+    if (jqXHR.readyState === 0 && jqXHR.status === 0) {
+      $('.message')
+      .text('You are not logged in. ')
+      .append('<a href="http://141.66.8.240:8081/aas/persons/current">Login</a>')
+      .append(' and click the back button to return to this page.');
+    }
+  });
+};
+
+var findPersonById = function(id) {
+  $.ajax({
+    type: 'GET',
+    url: AAS_URI + id,
+    dataType: 'json',
+    xhrFields: {
+      withCredentials: true
+    },
+    username: 'admin',
+    password: 'admin',
+    statusCode: {
+      404: function() {
+        console.log('page not found.');
+      },
+      401: function() {
+        console.log('unauthorized.');
+      }
+    }
+  })
+  .done(function(data) {
+    console.log('data: ', data);
+    $('.message').append('<div>' + JSON.stringify(data) + '</div>');
+    $('.message').append('<div>Updating snoopy...</div>');
+    data.title = 'msc';
+    console.log(data.title);
+    updatePerson(data);
+  })
+  .error(function(jqXHR, textStatus, errorThrown) {
+    console.log('error: ', textStatus);
+    console.log(jqXHR);
+    $('#aas').append(jqXHR.statusText + '\n');
+  })
+  .always(function(jqXHR) {
+    console.log('Find person by id finished: ', jqXHR);
+  });
+};
+
+var updatePerson = function(data) {
+  $.ajax({
+    type: 'PUT',
+    url: AAS_URI + data.id,
+    data: data,
+    contentType: 'application/json',
+    dataType: 'json',
+    xhrFields: {
+      withCredentials: true
+    }
+  })
+  .done(function(data) {
+    console.log('done. ', data);
+    $('.message').append('<div>' + data.id + ' is updated</div>');
+    $('.message').append('<div>' + JSON.stringify(data) + '</div>');
+  })
+  .always(function(jqXHR) {
+    console.log('Send PUT Request is finished: ', jqXHR);
+  });
+};
+
+var getGithubRequest = function() {
+  var REST_URI = 'https://api.github.com';
+  var xhr = $.ajax({
+    url: REST_URI,
+    dataType: 'json'
+  })
+  .done(function(data) {
+    console.log('data: ', data);
+    $('#github').append('success');
+  })
+  .error(function(data) {
+    console.log('error: ', data);
+    $('#github').append(data.statusText + '\n');
+  })
+  .always(function(data) {
+    console.log('finsihed: ', data);
+  });
+};
